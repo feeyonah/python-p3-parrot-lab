d8db1109d7f5528f56a74b210488038b9a53037b
fiona <omburafiona@gmail.com>
2023-10-12 04:03:01 +0300

    parrot done

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,6 @@
-def parrot():
-    pass
+def parrot(string="Squawk!"):
+    print(string)
+    return(string)
+#alternatives 
+return1=parrot()
+return2=parrot("hey Fiona!,I think you are really awesome")
